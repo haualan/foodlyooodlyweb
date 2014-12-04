706742de695daa09c29e58021d1be94a3ceceb7f
haualan <alan.wc.hau@gmail.com>
2014-12-04 16:42:36 -0500

    ..

--- config/config.js
+++ config/config.js
@@ -17,14 +17,14 @@ module.exports = _.extend(
 
 
 // If we are in local development environment the oath2 redirect_uris need to change
-var isLocalEnv = true;
+var isLocalEnv = false;
 // insert oauth2 tokens here
 
 if (isLocalEnv) {
 	module.exports.google = JSON.parse('{"web":{"auth_uri":"https://accounts.google.com/o/oauth2/auth","client_secret":"4_RUgxmczZP3xilqIM9vfU3t","token_uri":"https://accounts.google.com/o/oauth2/token","client_email":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","redirect_uris":["http://localhost:3000/auth/google/callback"],"client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","client_id":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq.apps.googleusercontent.com","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","javascript_origins":["https://localhost:3000"]}}');
 
 } else {
-	module.exports.google = JSON.parse('{"web":{"auth_uri":"https://accounts.google.com/o/oauth2/auth","client_secret":"4_RUgxmczZP3xilqIM9vfU3t","token_uri":"https://accounts.google.com/o/oauth2/token","client_email":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","redirect_uris":["http://104.131.123.186:3000/auth/google/callback"],"client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","client_id":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq.apps.googleusercontent.com","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","javascript_origins":["https://104.131.123.186:3000"]}}');
+	module.exports.google = JSON.parse('{"web":{"auth_uri":"https://accounts.google.com/o/oauth2/auth","client_secret":"4_RUgxmczZP3xilqIM9vfU3t","token_uri":"https://accounts.google.com/o/oauth2/token","client_email":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","redirect_uris":["http://alanhau.me/auth/google/callback"],"client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/723630520056-p4d0s4le60cpremdk6vovsfddum2nesq@developer.gserviceaccount.com","client_id":"723630520056-p4d0s4le60cpremdk6vovsfddum2nesq.apps.googleusercontent.com","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","javascript_origins":["https://alanhau.me"]}}');
 
 }
 
