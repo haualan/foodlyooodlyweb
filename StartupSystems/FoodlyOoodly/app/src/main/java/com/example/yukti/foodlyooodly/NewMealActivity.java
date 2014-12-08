647fec71f85a272d1fe8e00e607071dc9bfe643c
Yukti <yukti.abrol@gmail.com>
2014-12-08 18:58:48 -0500

    camera working

--- StartupSystems/FoodlyOoodly/app/src/main/java/com/example/yukti/foodlyooodly/NewMealActivity.java
+++ StartupSystems/FoodlyOoodly/app/src/main/java/com/example/yukti/foodlyooodly/NewMealActivity.java
@@ -1,7 +1,9 @@
 package com.example.yukti.foodlyooodly;
 
+import android.app.Activity;
+
 /**
  * Created by Yukti on 12/8/14.
  */
-public class NewMealActivity {
+public class NewMealActivity extends Activity{
 }
